2755a0bb94fd9551a207277c4d26231273aadc28
Nick Gromyko <nxgr.dev@gmail.com>
2022-11-29 20:49:15 +0600

    test .catch

--- src/services/CommentParser.js
+++ src/services/CommentParser.js
@@ -3,21 +3,26 @@ const axios = require("axios"),
 
 module.exports = {
     getComments: async () => {
-        let comments = await axios.get(mapUrl).data
-        let commentsList = []
-        console.log(comments)
-        comments.forEach( item => {
-            const encodedCoords = encodeURIComponent(item.location.coordinates.join(','))
-            commentsList.push({
-                id: item.id,
-                comment: item.comment,
-                location: `https://2gis.kz?m=${encodedCoords}%2f15&traffic`,
-                user: item.user.name,
-                feedback: item.feedbacks,
-                timestamp: new Date(item.timestamp * 1000)
-            })
+        await axios.get(mapUrl).then(res => {
+            console.log(res)
         })
-        return commentsList
+            .catch(err => {
+                console.log(err)
+            })
+        // let commentsList = []
+        // console.log(comments)
+        // comments.forEach( item => {
+        //     const encodedCoords = encodeURIComponent(item.location.coordinates.join(','))
+        //     commentsList.push({
+        //         id: item.id,
+        //         comment: item.comment,
+        //         location: `https://2gis.kz?m=${encodedCoords}%2f15&traffic`,
+        //         user: item.user.name,
+        //         feedback: item.feedbacks,
+        //         timestamp: new Date(item.timestamp * 1000)
+        //     })
+        // })
+        // return commentsList
     }
 
 }
